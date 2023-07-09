5ade0efd992851a022f05feb79910b0529225e1d
joelnyongesa <joelnyongesa148@gmail.com>
2023-07-09 12:43:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {};
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    employee = {...employee}
+    employee[key] = value;
+    return employee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    employee = {...employee};
+    delete employee[key];
+    return employee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
